b979d63338cb80c5cb44fa921b77e3390e6cc590
taotaoaini <3523184510@qq.com>
2024-08-28 19:34:20 +0800

    feat: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <h2>webpack-vue3-ts</h2>
+    <img :src="smallImg" alt="小于10kb的图片"/>
+    <img :src="bigImg" alt="大于10kb的图片"/>
+    <h2>webpack</h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import '@/app.css'
+import '@/app.scss'
+import smallImg from '@/assets/imgs/666.jpg'
+import  bigImg from '@/assets/imgs/pic4.jpg'
+</script>
+
+<style scoped>
+
+</style>
