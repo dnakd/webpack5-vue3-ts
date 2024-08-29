2a0b6ff8fda3c80801a2db623e6162bab8e74abd
taotaoaini <3523184510@qq.com>
2024-08-29 09:21:42 +0800

    feat 初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 // .eslintrc.js 示例
 module.exports = {
-    'env': {
-        'browser': true, 'es2021': true, 'node': true,
+    root:true,
+    env: {
+        'node': true,
     },
-    'extends': ['eslint:recommended', 'plugin:vue/vue3-essential'],
-    'parser': ['babel-eslint', 'vue-eslint-parser'],
-    'parserOptions': {
+    extends: ['eslint:recommended', 'plugin:vue/vue3-essential'],
+    parserOptions: {
         'ecmaVersion': '2020', 'sourceType': 'module',
     },
-    'rules': {
+    rules: {
         'semi': ['error', 'always'], 'quotes': ['error', 'single'],
-        'no-unused-vars':['error', 'never'],
+        'no-unused-vars':['error'],
+        'vue/multi-word-component-names':'off',
     },
-    'plugins': ['vue'],
-    'globals': {}
 };
